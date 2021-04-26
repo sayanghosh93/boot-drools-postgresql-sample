2815c38e62c002437e0cc33ceaa2ae5867e226ac
Sayan Ghosh <sayan.ghosh@intelligentappsolutions.com>
2021-04-26 11:16:45 +0530

    Initial Commit

---
+++ src/main/java/com/sayan/tcsdummy/TcsDummyApplication.java
@@ -0,0 +1,13 @@
+package com.sayan.tcsdummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcsDummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcsDummyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayan/tcsdummy/TcsDummyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayan.tcsdummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcsDummyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
